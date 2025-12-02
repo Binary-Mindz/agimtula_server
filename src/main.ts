124d426774fe91a7eb9386cc9b400adcbf7a6a5c
mdsharif <srka780@gmail.com>
2025-12-03 05:02:22 +0600

    fix db connet system

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   SwaggerSetting(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();

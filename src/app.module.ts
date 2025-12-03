622ab5ab921d5cb645d8c1d991b2cd0d0d74b924
mdsharif <srka780@gmail.com>
2025-12-04 05:48:50 +0600

    file setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImapApisModule } from './imap-apis/imap-apis.module';
+import { DatabaseModule } from './config/database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminRouteModule } from './admin-route/admin-route.module';
+import { AccountantRouteModule } from './accountant-route/accountant-route.module';
+import { UserRouteModule } from './user-route/user-route.module';
 
 @Module({
-  imports: [ImapApisModule],
+  imports: [
+    DatabaseModule,
+    ImapApisModule,
+    AuthModule,
+    AdminRouteModule,
+    AccountantRouteModule,
+    UserRouteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

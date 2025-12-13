3295d3b112c8a03ad1bf3fff363af64799cd0177
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-12-14 04:01:56 +0600

    changed migration

--- prisma/generated/prisma/client.ts
+++ prisma/generated/prisma/client.ts
@@ -88,6 +88,11 @@ export type SubscriptionPlan = Prisma.SubscriptionPlanModel
  */
 export type PackagePricing = Prisma.PackagePricingModel
 /**
+ * Model realtimeSelectionTime
+ * 
+ */
+export type realtimeSelectionTime = Prisma.realtimeSelectionTimeModel
+/**
  * Model User
  * 
  */

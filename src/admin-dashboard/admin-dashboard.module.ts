5110335afca5c0f5447cb3a4e6840732a8c20f13
mdsharif <srka780@gmail.com>
2025-12-21 01:56:05 +0600

    subscriptionPlanList

--- src/admin-dashboard/admin-dashboard.module.ts
+++ src/admin-dashboard/admin-dashboard.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-import { UsersModule } from './users/users.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { UserManagementModule } from './user-management/user-management.module';
 
 @Module({
-  imports: [SubscriptionsModule, UsersModule, DashboardModule, UserManagementModule],
+  imports: [SubscriptionsModule, DashboardModule, UserManagementModule],
 })
 export class AdminDashboardModule {}
--- src/admin-dashboard/subscriptions/subscriptions.service.ts
+++ src/admin-dashboard/subscriptions/subscriptions.service.ts
@@ -63,6 +63,7 @@ export class SubscriptionsService {
             id: true,
             price: true,
             setupFee: true,
+            planFeatures: true,
             billingPeriod: true,
           },
         },

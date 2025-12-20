6701533456b98863b0e82cab9e6a715ac66fc4d2
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-12-21 05:46:40 +0600

    Auto commit with changes: M	src/admin-dashboard/admin-dashboard.module.ts A	src/admin-dashboard/reports-and-analytics/dto/create-reports-and-analytic.dto.ts A	src/admin-dashboard/reports-and-analytics/dto/update-reports-and-analytic.dto.ts A	src/admin-dashboard/reports-and-analytics/entities/reports-and-analytic.entity.ts A	src/admin-dashboard/reports-and-analytics/reports-and-analytics.controller.ts A	src/admin-dashboard/reports-and-analytics/reports-and-analytics.module.ts A	src/admin-dashboard/reports-and-analytics/reports-and-analytics.service.ts M	src/auth/auth.service.ts

--- src/admin-dashboard/admin-dashboard.module.ts
+++ src/admin-dashboard/admin-dashboard.module.ts
@@ -3,8 +3,9 @@ import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { UsersModule } from './users/users.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { UserManagementModule } from './user-management/user-management.module';
+import { ReportsAndAnalyticsModule } from './reports-and-analytics/reports-and-analytics.module';
 
 @Module({
-  imports: [SubscriptionsModule, UsersModule, DashboardModule, UserManagementModule],
+  imports: [SubscriptionsModule, UsersModule, DashboardModule, UserManagementModule, ReportsAndAnalyticsModule],
 })
 export class AdminDashboardModule {}

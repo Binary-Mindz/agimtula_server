7620aeb7294e61b5c5cd46b065930b114d9e0f48
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-12-23 01:53:48 +0600

    added some fetching tech

--- src/user-dashboard/reports/reports.service.ts
+++ src/user-dashboard/reports/reports.service.ts
@@ -6,31 +6,54 @@ export class ReportsService {
   constructor(private prisma: PrismaService) {}
 
   async getReportData(userId: string) {
-    const [receipt, mileage] = await Promise.all([
+    const sixMonthsAgo = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth() - 5,
+      1,
+    );
+    const [totalReceipt, totalMileage, receipt, mileage] = await Promise.all([
       this.prisma.receipt.aggregate({
-        where: {
-          userId,
-        },
+        where: { userId, createdAt: { gte: sixMonthsAgo } },
         _sum: { amount: true },
       }),
       this.prisma.mileage.aggregate({
-        where: {
-          userId,
-        },
+        where: { userId },
         _sum: { amount: true },
       }),
 
+      this.prisma.receipt.findMany({
+        where: { userId, createdAt: { gte: sixMonthsAgo } },
+        select: { amount: true, createdAt: true },
+      }),
+      this.prisma.mileage.findMany({
+        where: {
+          userId,
+        },
+        select: { amount: true },
+      }),
     ]);
 
-    const receiptTotal = receipt._sum.amount || 0;
-    const mileageTotal = mileage._sum.amount || 0;
+    const all = [...receipt, mileage];
 
-    const report = {
-      expenseSummury: receiptTotal + mileageTotal,
-      receiptTotal,
-      mileageTotal,
-    };
+      const monthlySummary: { year: number; month: number; total: number }[] = [];
+      
+    //   for (const item of all) {
+    //     const year = item.createdAt.getFullYear();
+    //     const month = item.createdAt.getMonth() + 1;
 
-    // monthly receipt data
+    //     const existingEntry = monthlySummary.find(
+    //       (entry) => entry.year === year && entry.month === month,
+    //     );
+
+    //     if (existingEntry) {
+    //       existingEntry.total += item.amount;
+    //     } else {
+    //       monthlySummary.push({
+    //         year,
+    //         month,
+    //         total: item.amount,
+    //       });
+    //     }
+    //   }
   }
 }

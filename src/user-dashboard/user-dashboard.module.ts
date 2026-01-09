468d66b1e2aa2c28b539d4bb59dfd9d9c3e0ca11
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2026-01-10 00:05:29 +0600

    modified user module

--- src/user-dashboard/user-dashboard.module.ts
+++ src/user-dashboard/user-dashboard.module.ts
@@ -6,8 +6,9 @@ import { ReportsModule } from './reports/reports.module';
 import { RequesteAccountantModule } from './request-accountant/request-accountant.module';
 import { InvoicesModule } from './invoices/invoices.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { SupportModule } from './support/support.module';
 
 @Module({
-  imports: [AutoInvoiceImportsModule, ReceiptsModule, PaymentModule, ReportsModule, RequesteAccountantModule, InvoicesModule, DashboardModule],
+  imports: [AutoInvoiceImportsModule, ReceiptsModule, PaymentModule, ReportsModule, RequesteAccountantModule, InvoicesModule, DashboardModule, SupportModule],
 })
 export class UserDashboardModule { }
